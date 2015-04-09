3c682cba896b5575693fb29f9f1c243ab9592a51
Puneet Behl <puneet.behl@intelligrape.com>
2015-04-09 21:31:34 +0530

    Implemented test cases for named query example.

--- test/integration/com/sampleapp/PublicationIntegrationSpec.groovy
+++ test/integration/com/sampleapp/PublicationIntegrationSpec.groovy
@@ -1,10 +1,26 @@
 package com.sampleapp
 
 import spock.lang.Specification
+import grails.buildtestdata.mixin.Build
 
+@Build(Publication)
 class PublicationIntegrationSpec extends Specification {
 
 	def setup() {
-		
+		Date currentDate = new Date() - 10
+		10?.times { i ->
+			Publication.build(datePublished: currentDate)
+			currentDate = currentDate + 1
+		}
+	}
+
+	def "canary test to see books are created from build test data plugin"() {
+		expect: 
+		Publication.count() == 10
+	}
+
+	def "recent publication count shoud be 7"() {
+		expect:
+		Publication.recentPublications().count() == 6
 	}
 }
